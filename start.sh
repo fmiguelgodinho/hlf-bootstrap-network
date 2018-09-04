b95727884027dcde16ef90ec9734240f68b2d58b
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-09-04 14:20:55 +0100

    centralized bc

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ echo "2. Peers will start joining channel $CHANNEL_NAME"
 sleep 15
 
 # peer joins channel
-for l in {a..t}; do
+for l in {a..d}; do
   L=${l^^}
   docker exec \
   -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/blockchain-${l}.com/peers/peer0.blockchain-${l}.com/tls/ca.crt" \
@@ -47,7 +47,7 @@ done
 echo "3. Updating channel $CHANNEL_NAME information"
 sleep 15
 
-for l in {a..t}; do
+for l in {a..d}; do
   L=${l^^}
   docker exec \
   -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/blockchain-${l}.com/peers/peer0.blockchain-${l}.com/tls/ca.crt" \
@@ -61,13 +61,13 @@ done
 
 # chaincode deployment
 export CHAINCODE_FILENAME_NOEXT=xcc
-export CHAINCODE_ENDORSEMENT_POLICY="OR('PeersAMSP.member','PeersBMSP.member','PeersCMSP.member','PeersDMSP.member','PeersEMSP.member','PeersFMSP.member','PeersGMSP.member','PeersHMSP.member','PeersIMSP.member','PeersJMSP.member','PeersKMSP.member','PeersLMSP.member','PeersMMSP.member','PeersNMSP.member','PeersOMSP.member','PeersPMSP.member','PeersQMSP.member','PeersRMSP.member','PeersSMSP.member','PeersTMSP.member')"
-export CHAINCODE_INSTANTIATE_ARGS='{"Args":["{\"contract-id\":\"xcc\",\"contract-version\":1,\"available-functions\":[[\"type:query\",\"fn:query\",\"arg0:key(string)\",\"arg1:viewHistory(bool)\"],[\"type:query\",\"fn:queryAll\"],[\"type:invoke\",\"fn:put\",\"arg0:key(string)\",\"arg1:value(string)\"]],\"installed-on-nodes\":[\"peer0.blockchain-a.com\",\"peer0.blockchain-b.com\",\"peer0.blockchain-c.com\",\"peer0.blockchain-d.com\",\"peer0.blockchain-e.com\",\"peer0.blockchain-f.com\",\"peer0.blockchain-g.com\",\"peer0.blockchain-h.com\",\"peer0.blockchain-i.com\",\"peer0.blockchain-j.com\",\"peer0.blockchain-k.com\",\"peer0.blockchain-l.com\",\"peer0.blockchain-m.com\",\"peer0.blockchain-n.com\",\"peer0.blockchain-o.com\",\"peer0.blockchain-p.com\",\"peer0.blockchain-q.com\",\"peer0.blockchain-r.com\",\"peer0.blockchain-s.com\",\"peer0.blockchain-t.com\"],\"channel\":\"mainchannel\",\"signature-type\":\"threshsig\",\"expires-on\":\"2019-12-31T23:59:59\",\"valid-from\":\"2018-01-01T08:00:00\",\"signing-nodes\":[{\"name\":\"peer0.blockchain-a.com\",\"host\":\"51.255.64.183\",\"port\":7051,\"event-hub-port\":7053},{\"name\":\"peer0.blockchain-b.com\",\"host\":\"51.255.64.183\",\"port\":10051,\"event-hub-port\":10053},{\"name\":\"peer0.blockchain-d.com\",\"host\":\"51.255.64.183\",\"port\":9051,\"event-hub-port\":9053},{\"name\":\"peer0.blockchain-g.com\",\"host\":\"51.255.64.183\",\"port\":13051,\"event-hub-port\":13053},{\"name\":\"peer0.blockchain-k.com\",\"host\":\"51.255.64.183\",\"port\":26051,\"event-hub-port\":26053},{\"name\":\"peer0.blockchain-o.com\",\"host\":\"51.255.64.183\",\"port\":20051,\"event-hub-port\":20053},{\"name\":\"peer0.blockchain-t.com\",\"host\":\"51.255.64.183\",\"port\":25051,\"event-hub-port\":25053}],\"consensus-type\":\"bft\",\"consensus-nodes\":[{\"name\":\"orderer0.consensus.com\",\"host\":\"51.255.64.183\",\"port\":7050},{\"name\":\"orderer1.consensus.com\",\"host\":\"51.255.64.183\",\"port\":8050},{\"name\":\"orderer2.consensus.com\",\"host\":\"51.255.64.183\",\"port\":9050},{\"name\":\"orderer3.consensus.com\",\"host\":\"51.255.64.183\",\"port\":10050}]}","{\"max-records\":100,\"total-records\":0}"]}'
+export CHAINCODE_ENDORSEMENT_POLICY="OR('PeersDMSP.member')"
+export CHAINCODE_INSTANTIATE_ARGS='{"Args":["{\"contract-id\":\"xcc\",\"contract-version\":1,\"available-functions\":[[\"type:query\",\"fn:query\",\"arg0:key(string)\",\"arg1:viewHistory(bool)\"],[\"type:query\",\"fn:queryAll\"],[\"type:invoke\",\"fn:put\",\"arg0:key(string)\",\"arg1:value(string)\"]],\"installed-on-nodes\":[\"peer0.blockchain-a.com\",\"peer0.blockchain-b.com\",\"peer0.blockchain-c.com\",\"peer0.blockchain-d.com\",\"peer0.blockchain-e.com\",\"peer0.blockchain-f.com\",\"peer0.blockchain-g.com\",\"peer0.blockchain-h.com\",\"peer0.blockchain-i.com\",\"peer0.blockchain-j.com\",\"peer0.blockchain-k.com\",\"peer0.blockchain-l.com\",\"peer0.blockchain-m.com\",\"peer0.blockchain-n.com\",\"peer0.blockchain-o.com\",\"peer0.blockchain-p.com\",\"peer0.blockchain-q.com\",\"peer0.blockchain-r.com\",\"peer0.blockchain-s.com\",\"peer0.blockchain-t.com\"],\"channel\":\"mainchannel\",\"signature-type\":\"multisig\",\"expires-on\":\"2019-12-31T23:59:59\",\"valid-from\":\"2018-01-01T08:00:00\",\"signing-nodes\":[{\"name\":\"peer0.blockchain-a.com\",\"host\":\"51.255.64.183\",\"port\":7051,\"event-hub-port\":7053},{\"name\":\"peer0.blockchain-b.com\",\"host\":\"51.255.64.183\",\"port\":10051,\"event-hub-port\":10053},{\"name\":\"peer0.blockchain-d.com\",\"host\":\"51.255.64.183\",\"port\":9051,\"event-hub-port\":9053},{\"name\":\"peer0.blockchain-g.com\",\"host\":\"51.255.64.183\",\"port\":13051,\"event-hub-port\":13053},{\"name\":\"peer0.blockchain-k.com\",\"host\":\"51.255.64.183\",\"port\":26051,\"event-hub-port\":26053},{\"name\":\"peer0.blockchain-o.com\",\"host\":\"51.255.64.183\",\"port\":20051,\"event-hub-port\":20053},{\"name\":\"peer0.blockchain-t.com\",\"host\":\"51.255.64.183\",\"port\":25051,\"event-hub-port\":25053}],\"consensus-type\":\"bft\",\"consensus-nodes\":[{\"name\":\"orderer0.consensus.com\",\"host\":\"51.255.64.183\",\"port\":7050},{\"name\":\"orderer1.consensus.com\",\"host\":\"51.255.64.183\",\"port\":8050},{\"name\":\"orderer2.consensus.com\",\"host\":\"51.255.64.183\",\"port\":9050},{\"name\":\"orderer3.consensus.com\",\"host\":\"51.255.64.183\",\"port\":10050}]}","{\"max-records\":100,\"total-records\":0}"]}'
 
 echo "4. Installing chaincode $CHAINCODE_FILENAME_NOEXT on peers"
 
 # install chaincode on peer
-for l in {a..t}; do
+for l in {a..d}; do
   L=${l^^}
   docker exec \
   -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/blockchain-${l}.com/peers/peer0.blockchain-${l}.com/tls/ca.crt" \
@@ -93,7 +93,7 @@ sleep 15
 echo "6. Querying all peers to trigger chaincode for the first time!"
 
 # query chaincode on each peer to fire up its chaincode container
-for l in {a..t}; do
+for l in {a..d}; do
   L=${l^^}
   docker exec \
   -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/blockchain-${l}.com/peers/peer0.blockchain-${l}.com/tls/ca.crt" \
